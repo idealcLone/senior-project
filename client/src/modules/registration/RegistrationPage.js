43371e9b7e6a233abec3aa0524da53ec7675e357
idealcLone <eldar.mussagali@nu.edu.kz>
2022-02-07 12:32:05 +0600

    Prettier test

--- client/src/modules/registration/RegistrationPage.js
+++ client/src/modules/registration/RegistrationPage.js
@@ -2,5 +2,10 @@ import React from 'react';
 
 export const RegistrationPage = () => {
   const [courses, setCourses] = React.useState([])
+
+  const func = (a) => {
+    return a
+  }
+
   return <div>Registration Page</div>
 };
